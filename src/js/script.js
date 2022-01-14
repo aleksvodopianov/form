f88ea2c6baf9b3a59e0e4e728c720b80a5a60a1d
Aleks <vodopyanov3d@gmail.com>
2022-01-14 21:06:50 +0200

    created the project structure

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// Next function checks browser support WEBP format ********************************************************************************************************
+function testWebP(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src =
+        'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+        console.log('use webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+        console.log('not use webp');
+    }
+});
